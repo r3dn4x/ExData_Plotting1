6d65669abd9896efe0f5f7ea8c9b6d686b424be6
r3dn4x <arguello@gmail.com>
2015-05-09 21:41:47 -0400

    test

---
+++ plot2.R
@@ -0,0 +1,24 @@
+#Download the data file
+
+if (!file.exists("data")){
+        dir.create("data")
+}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./data/power.zip", method = "curl")
+unzip("./data/power.zip", exdir = "./data")
+
+#Read the data file, select only the relevant dates and replace the Date and
+#Time columns with a new DateTime column of the right date class.
+
+powerData <- read.table("./data/household_power_consumption.txt",
+                        header = TRUE, sep = ";",
+                        stringsAsFactors = FALSE)
+
+library(dplyr)          #Make sure the necessary packages are loaded.
+library(lubridate)
+
+data <- powerData %>% filter(Date == "1/2/2007" | Date == "2/2/2007") %>% 
+        mutate(DateTime = dmy_hms(paste(Date, Time))) %>% 
+        select(DateTime, c(3:9)) %>%
+        mutate_each(funs(as.numeric(.)), c(2:8))
